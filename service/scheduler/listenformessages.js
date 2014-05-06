11b99d4bfb9179051092ff10919986de2aef0f6b
windowsazure <windowsazure>
2014-05-06 00:26:01 +0000

    Committing update from request /scmvfs/service/scheduler/listenForMessages.js

--- service/scheduler/listenformessages.js
+++ service/scheduler/listenformessages.js
@@ -1,3 +1,17 @@
+//import the azure module
+var azure = require('azure');
+var c_Timeout = 60;
+
 function listenForMessages() {
-    console.warn("You are running an empty scheduled job. Update the script for job 'listenForMessages' or disable the job.");
+   console.log('Started new listener job');
+   
+   var date = new Date();
+   var time = date.getTime();
+   //get the current unix time in seconds
+   var startSeconds = time / 1000;
+  
+   
+   console.log(process.env.ServiceBusConnString);
+   var sb = azure.createServiceBusService(process.env.ServiceBusConnString);
+   
 }
